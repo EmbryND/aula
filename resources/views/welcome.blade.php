a0307c607e1758040ed7eba07b5b0c65cfb2cfb2
EmbryND <elvislimasamuel27@gmail.com>
2025-02-20 22:38:21 -0300

    Projeto Loja Games

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{url('cssLoja/loja.css')}}">
+</head>
+<body>
+    <header>ArMarioGames</header>
+    <main>
+        <h2 class="secao_sony">Vídeo-games da Sony</h2>
+        <div class="produtos_sony">
+            <div>
+            <img src="https://images-americanas.b2w.io/produtos/4646885354/imagens/sony-ps-vita-slim-1gb-novo-lacrado/4646885362_1_large.jpg" alt="PSVita">
+            <p class="nome_sony">Sony PSVita Slim 1GB Novo Lacrado</p>
+            <p class="preco_sony">R$2.299,00</p>
+            </div>
+            <div>
+                <img src="https://http2.mlstatic.com/D_NQ_NP_2X_923283-MLA40388393976_012020-F.webp" alt="PS2">
+                <p class="nome_sony">Sony PlayStation 2 Slim Standard Charcoal black</p>
+                <p class="preco_sony">R$579,99</p>
+            </div>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTpKb4oTgqXSILru2xGUE6omHA3JgTPwc3Hra-qZxMHW3wGdN1kW2MvEP6ziQB5gI-sX3TIW2ZkCmCqM3E5Q39ro9fxap6Veka6UHQuUEywFNTfT0D02FHM&usqp=CAE" alt="PS5"> 
+                <p class="nome_sony">Sony Playstation Console PS5 Digital Slim</p>
+                <p class="preco_sony">R$2.499,99</p>
+            </div>
+    </div>
+    </main>
+</body>
+</html>
