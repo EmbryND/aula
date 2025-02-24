69b2c2d9381638e2dc41edecea14c6d8ad1ce3dc
EmbryND <elvislimasamuel27@gmail.com>
2025-02-24 20:41:13 -0300

    Adição de seções para novos consoles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,22 +9,58 @@
 <body>
     <header>ArMarioGames</header>
     <main>
-        <h2 class="secao_sony">Vídeo-games da Sony</h2>
-        <div class="produtos_sony">
+        <h2 class="secao_playstation">Vídeo-games PlayStation</h2>
+        <div class="produtos_playstation">
             <div>
             <img src="https://images-americanas.b2w.io/produtos/4646885354/imagens/sony-ps-vita-slim-1gb-novo-lacrado/4646885362_1_large.jpg" alt="PSVita">
-            <p class="nome_sony">Sony PSVita Slim 1GB Novo Lacrado</p>
-            <p class="preco_sony">R$2.299,00</p>
+            <p class="nome_playstation">Sony PSVita Slim 1GB Novo Lacrado</p>
+            <p class="preco_playstation">R$2.299,00</p>
             </div>
             <div>
                 <img src="https://http2.mlstatic.com/D_NQ_NP_2X_923283-MLA40388393976_012020-F.webp" alt="PS2">
-                <p class="nome_sony">Sony PlayStation 2 Slim Standard Charcoal black</p>
-                <p class="preco_sony">R$579,99</p>
+                <p class="nome_playstation">Sony PlayStation 2 Slim Standard Charcoal black</p>
+                <p class="preco_playstation">R$579,99</p>
             </div>
             <div>
                 <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTpKb4oTgqXSILru2xGUE6omHA3JgTPwc3Hra-qZxMHW3wGdN1kW2MvEP6ziQB5gI-sX3TIW2ZkCmCqM3E5Q39ro9fxap6Veka6UHQuUEywFNTfT0D02FHM&usqp=CAE" alt="PS5"> 
-                <p class="nome_sony">Sony Playstation Console PS5 Digital Slim</p>
-                <p class="preco_sony">R$2.499,99</p>
+                <p class="nome_playstation">Sony Playstation Console PS5 Digital Slim</p>
+                <p class="preco_playstation">R$2.499,99</p>
+            </div>
+    </div>
+    <h2 class="secao_xbox">Vídeo-games Xbox</h2>
+        <div class="produtos_xbox">
+            <div>
+            <img src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ5-u0VoDge-mBpAeEpBRfMGSSYYwB0xvGOLcNAbIJeIuDl6dvC6FvKRYQ2VxTzclZ5WjEIFPYlAOZ9d1SATQIi3blTGODN5khVOWNIXOJOrHmLud6Cq2P1sg&usqp=CAE" alt="Xbox Series">
+            <p class="nome_xbox">Console Xbox Series S 512GB Branco</p>
+            <p class="preco_xbox">R$2.599,00</p>
+            </div>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRwsiZvYa-Mlf4DLnajLfzIueu8s0SCB5z3ULRqzuXGAog2hZnuU5hpku_h2upIjRhYKk4CVMcGqHXR0LE3ejuq6IWFBlRgqb73txLALTNhV5aUuI7ADfhpyw&usqp=CAE" alt="Xbox 360">
+                <p class="nome_xbox">Console Xbox 360 Slim 4GB - Microsoft</p>
+                <p class="preco_xbox">R$629,91</p>
+            </div>
+            <div>
+                <img src="https://cdn.awsli.com.br/300x300/1258/1258259/produto/70079984/0f00be7f96.jpg" alt="Xbox One X"> 
+                <p class="nome_xbox">Console Xbox One X 1TB - Microsoft</p>
+                <p class="preco_xbox">R$1.990,00</p>
+            </div>
+    </div>
+    <h2 class="secao_nintendo">Vídeo-games Nintendo</h2>
+        <div class="produtos_nintendo">
+            <div>
+            <img src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTmeKeCTZOdCo2DDwmfjFj4joBNAHZf290PqF-jjgoM7iEFGCJChtn58VnAeNoCkRanq6RLaiyzI-lvGj5BzBWBm79o8kT6&usqp=CAE" alt="Wii U">
+            <p class="nome_nintendo">Console Nintendo Wii U, Deluxe, 32GB, Preto</p>
+            <p class="preco_nintendo">R$1.199,00</p>
+            </div>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT3C9QJLbRii3yWYkR1ECUJfYMqIsrCjo2jhEEGg3dECbzVVindw2OYRo96Tj0jW8pmB3xysPMXUo7tVtZwDyB-t884_tUz1lTJxNeC2VE&usqp=CAE" alt="Nintendo Switch">
+                <p class="nome_nintendo">Console Nintendo Switch 32gb Mario Kart 8 Deluxe</p>
+                <p class="preco_nintendo">R$1.1990,00</p>
+            </div>
+            <div>
+                <img src="https://i5.walmartimages.com/seo/Nintendo-64-N64-System-Console-Used_3f05ad58-eab0-45ab-a66a-03d99113a617_1.4854fae622648fca1b9163c24ba59e13.jpeg" alt="Nintendo 64"> 
+                <p class="nome_nintendo">Console Nintendo 64</p>
+                <p class="preco_nintendo">R$1.299,99</p>
             </div>
     </div>
     </main>
